38b6a6e7c7c69b0d82f3d9fab3e3d68a97a2a57e
Billy <13036151491@163.com>
2021-10-15 18:23:34 +0800

    删除注释

--- lib/AppRoot/AppMineWidget.dart
+++ lib/AppRoot/AppMineWidget.dart
@@ -5,7 +5,6 @@ import 'package:flutter_app/MineModule/MineMainWidget.dart';
 class AppMineWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    print('cccccc');
     return MineMainWidget();
   }
 }
